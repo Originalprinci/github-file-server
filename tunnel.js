53e9323c410a534d227720c3c5afe2f13c95c62b
Originalprinci <Original_principle@outlook.com>
2026-06-13 09:47:02 +0800

    Add tunnel.js for PM2 deployment

---
+++ tunnel.js
@@ -0,0 +1,6 @@
+const localtunnel = require('localtunnel');
+(async () => {
+  const tunnel = await localtunnel({ port: 3001, subdomain: 'githubfs-nied' });
+  console.log('github-file-server tunnel: ' + tunnel.url);
+  tunnel.on('close', () => console.log('Tunnel closed'));
+})();
